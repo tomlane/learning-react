6f979351a963e7d79ebf5cb2e984ec74e912dcb5
Tom Lane <tom@tomlane.me>
2017-08-22 01:14:50 +0100

    Use easy imports from NODE_PATH

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import logo from './images/logo.svg';
-import './App.css';
+import logo from 'images/logo.svg';
+import 'App.css';
 
 class App extends Component {
   render() {
