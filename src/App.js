fc748e94014a38ba729546f676d87b411eae182f
Tom Lane <tom@tomlane.me>
2017-08-22 01:10:08 +0100

    Structure changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -16,6 +12,10 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <small>
+            You are running this application in <b>{process.env.NODE_ENV}</b>{' '}
+            mode.
+          </small>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
